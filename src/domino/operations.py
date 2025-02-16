2201f0e1beef9c3512f07570673343d20067818d
denirz <denirz@gmail.com>
2025-02-16 15:47:32 +0300

    few Commnets added

--- src/domino/operations.py
+++ src/domino/operations.py
@@ -1,12 +1,14 @@
 from domino.pieces import Piece
-from copy import deepcopy
+from itertools import permutations
 def createpeice():
+    """ Итератор - создает последовательно все фишки"""
     for i in range(1,7):
         for j in range(i,7):
             if i == j: continue
             yield Piece(i,j)
 
 def createset():
+    """  Создает полный набор всех фишек """
     ps = set()
     for p in createpeice():
         ps.add(p)
@@ -14,10 +16,20 @@ def createset():
 
 
 def sum5(p1:Piece,p2:Piece,p3:Piece,p4:Piece,p5:Piece):
+    """Функция суммирует 5 фишек """
     res = p1.a/p1.b + p2.a/p2.b + p3.a/p3.b + p4.a/p4.b+ p5.a/p5.b
     return res
 
 def sum5options(p1:Piece,p2:Piece,p3:Piece,p4:Piece,p5:Piece):
+    """
+    Выбираем 5 фишек и проверяем  можно ли их пенревернуть так чтобы сумма их равнялась 10
+    :param p1:
+    :param p2:
+    :param p3:
+    :param p4:
+    :param p5:
+    :return:  5 верно  повернутых фишек или Nonе если фишки  не суммируются в 10
+    """
     for p1  in [p1,p1.flip()]:
         for p2 in [p2,p2.flip()]:
             for p3 in [p3,p3.flip()]:
@@ -26,22 +38,27 @@ def sum5options(p1:Piece,p2:Piece,p3:Piece,p4:Piece,p5:Piece):
                         d = sum5(p1,p2,p3,p4,p5)
                         # print(f"d={d}: {p1} {p2} {p3} {p4}")
                         if d >= 9.99 and d <= 10.01:
+                            # todo есть какой-то более красивый способ привести  Float в Int  и проверить что  это 10 , но я его сходу не знаю
                             return p1,p2,p3,p4,p5
-                            print(f"founded: {p1} + {p2} + {p3} + {p4} = {d}")
+                            # print(f"founded: {p1} + {p2} + {p3} + {p4} = {d}")
                         pass
                 pass
             pass
         pass
     return None
 
-from itertools import permutations
+
 def select5items(d):
-    # d=createset()
+    """
+    последовательно  возвращает все выборки по 5 фишек из заданного набора.  но только при условии что они в сумму могут дать 10(!)
+    :param d:
+    :return: set из 5 фишек
+    """
     s = permutations(d,5)
+    # todo  кажется что  тут возращаются еще и разнве перестановки а нам они не нужны. навернео есть более удачный вариант  permutation
+    # это сильно скорит  работу
     for i in s:
-        # print(i)
         if sum5options(*i):
-            # print(f"found: {i},sum={sum5(*i)}")
             yield i
 
 def splitpool(setitems,d):
